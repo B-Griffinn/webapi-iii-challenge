22d0dc6e80336570a54c5fa8fe9dbc978a98e262
Ben <bigriffin94@outlook.com>
2019-10-02 16:21:23 -0700

    MVP met

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+// GOTCHA require* was needed
+// Imports
+const express = require('express');
+const server = require('./server.js');
+
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`)
+  });
+
+const port = 5000;
+server.listen(port, () => {
+  console.log('\n*** Server is running on port 5000 ***\n')
+})
+
