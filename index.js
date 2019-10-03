0fd9e83648add9cf68393ce7fcb61d81cf8bfab2
Ben <bigriffin94@outlook.com>
2019-10-03 12:32:18 -0700

    Updates for Heroku

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ server.get('/', (req, res) => {
     res.send(`<h2>Let's write some middleware!</h2>`)
   });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 server.listen(port, () => {
   console.log('\n*** Server is running on port 5000 ***\n')
 })
